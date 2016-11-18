829ee3a7303a551bed56ea52f42addc14f7b0232
ramprabhu-idexcel <nramprabu@gmail.com>
2016-11-18 17:41:56 +0530

    API namespace

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,5 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
-  attributes :first_name, :last_name, :email, :age, :address, :phone, :created_at, :updated_at
+  attributes :id, :first_name, :last_name, :email, :age, :address, :phone, :created_at, :updated_at
 
   def created_at
     object.created_at.strftime("%d/%m/%Y")
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users do
-    post 'validate', on: :collection
+  namespace :api, defaults: {format: 'json'} do
+    resources :users do
+      collection do
+        post 'authenticate'
+        delete 'delete_all'
+      end
+    end
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
