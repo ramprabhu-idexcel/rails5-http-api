89614559ddb572220c26a84604aed2b80766d77f
ramprabhu-idexcel <nramprabu@gmail.com>
2016-11-21 17:52:06 +0530

    user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ApplicationRecord
   has_secure_token
   has_secure_password
 
-  validates :first_name, presence: true
+  validates :first_name, :last_name, :email, :username, :phone, :age, presence: true
+  validates :username, :email, uniqueness: true
 end
